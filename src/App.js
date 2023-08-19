fefc528805ecb20823d5c3b97f2a1756a84fb80d
Sojal agrawal <sojalagrawal2@gmail.com>
2023-08-19 18:45:09 +0530

    context api intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+
 import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import NoteState from './context/notes/NoteState';
+
 
 function App() {
   return (
-    <div className="App">
-      <h2> this is i Notebook</h2>
-    </div>
+    <>
+      <NoteState>
+      <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+      </Routes>
+      </NoteState>
+     
+   
+    </>
   );
 }
 
 export default App;
+
+
+
+
+// git remote add origin https://github.com/Sojalagrawal/backend-react.git
+// git branch -M main   
+// git push -u origin main
