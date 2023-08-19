385e228810fc80435ed7854c884cd47b344fe3df
Sojal agrawal <sojalagrawal2@gmail.com>
2023-08-19 11:14:05 +0530

    react started

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoURI="mongodb://127.0.0.1:27017/inoteDB";
 
 const connectToMongo=()=>{
     mongoose.connect("mongodb://127.0.0.1:27017/inoteDB",{useNewUrlParser:true});
-    // console.log("connected");
+    console.log("connected");
 }
 
 module.exports=connectToMongo
